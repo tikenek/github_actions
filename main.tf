236d5a25ef7af06e5ef33b330c6d0ccd31046902
Tynar Esenbaeva <68963500+tikenek@users.noreply.github.com>
2022-04-13 11:05:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,25 @@
-provider "aws" {
-  version = "= 3.74.2"
-  region  = "us-east-1"
-}
-
-
- terraform {
-   backend "s3" {
-     bucket = "tynaresen"
-     key    = "path/to/my/key"
-     region = "us-east-1"
-   }
- }
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tynaresen"
-
-  versioning {
-    enabled = true
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 3.0"
+    }
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tynaresen"
+    key            = "mykey/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+   bucket = "tynaresen"
+
+   versioning {
+     enabled = true
+  }
+}
+
+
