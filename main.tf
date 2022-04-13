96ccc8acbc4c4f98d7919f7e20e94e163ffc9130
Tynar Esenbaeva <68963500+tikenek@users.noreply.github.com>
2022-04-13 11:30:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
 provider "aws" {
-region = "us-east-1"
-profile = "tynar-admin"
-}
+   region     : ${{ secrets.AWS_DEFAULT_REGION }}
+   access_key : ${{ secrets.AWS_ACCESS_KEY_ID }}
+   secret_key : ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+} 
 
 
 terraform {
-    backend "s3" {
-    bucket = "tynaresen"
-    encrypt = true
-    key = "state.tfstate"
-    region = "us-east-1"
-    profile = "tynar-admin" 
+  backend "s3" {
+    bucket         = "tynaresen"
+    key            = "mykey/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+   bucket = "tynaresen"
+
+   versioning {
+     enabled = true
   }
 }
 
